6e166425ef97c9dd932a22c38b0c2ed1458dae07
Kevin Koontz <contactkkoontz@gmail.com>
2024-09-12 16:29:48 -0600

    PASSED TEST #1

--- src/services/BugsService.js
+++ src/services/BugsService.js
@@ -3,6 +3,7 @@ import { dbContext } from "../db/DbContext.js"
 class BugsService {
   async createBugs(bugData) {
     const bug = await dbContext.Bugs.create(bugData)
+    await bug.populate('creator')
     return bug
   }
 
