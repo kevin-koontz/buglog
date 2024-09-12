cc66c3a96b052d0e9bf4a1754221db7e0f6f4934
Kevin Koontz <contactkkoontz@gmail.com>
2024-09-12 14:52:16 -0600

    init

---
+++ www/env.js
@@ -0,0 +1,3 @@
+export const domain = 'dev-jvrs8qqzpjty2j8w.us.auth0.com'
+export const clientId = '9Z3XoKcdFEHGMbhKiSiwpmaUZtSo79NT'
+export const audience = 'https://auth.speedwagonfoundation.com'
