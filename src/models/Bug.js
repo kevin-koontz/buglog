d276923050c80d5f3db6d5a13c1f322a36c7ab3f
Kevin Koontz <contactkkoontz@gmail.com>
2024-09-12 15:19:44 -0600

    created bug model

--- src/models/Bug.js
+++ src/models/Bug.js
@@ -1,7 +1,23 @@
 import { Schema } from "mongoose";
 
-export const BugsSchema = new Schema({
+export const BugsSchema = new Schema(
+  {
+    title: { type: String, required: true, minlength: 10, maxlength: 50 },
+    description: { type: String, required: true, minlength: 10, maxlength: 500 },
+    priority: { type: Number, required: true, min: 1, max: 5 },
+    closed: { type: Boolean, required: true, default: false },
+    closedDate: { type: Date },
+    creatorId: { type: Schema.ObjectId, required: true, ref: 'Account' }
 
+  },
+  {
+    timestamps: true, toJSON: { virtuals: true }
+  }
+)
 
-},
-  timestamps: true, toJSON: { virtuals: true })
+BugsSchema.virtual('creator', {
+  localField: 'creatorId',
+  ref: 'Account',
+  foreignField: '_id',
+  justOne: true
+})
